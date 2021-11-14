26f8966958a234797f494f69cf4e8ccd9b6ac087
Diana <dnnsmnkozachenko@gmail.com>
2021-11-14 20:22:49 +0200

    replace world to dog

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[ ])
 {
 QApplication app(argc, argv);
  
-QPushButton button("Hello, NEW World!");
+QPushButton button("Hello, NEW dog!");
 button.resize(200, 60);
 button.show( );
  
